46a65a166e66a7d02f5f83378b532ebf8eb67f2d
Gursharn Singh <gursharn.singh@ampleorganics.com>
2018-11-29 10:23:45 -0500

    allow mocking a store module without getters

--- src/mockStoreActionsAndGetters.js
+++ src/mockStoreActionsAndGetters.js
@@ -3,20 +3,36 @@ function mockStoreActionsAndGetters ({
   mockedGetters,
   jestFn
 }) {
-  let actionsDictionary = {}
-  let gettersDictionary = {}
+  const actionsDictionary = {}
+  const gettersDictionary = {}
 
   Object.keys(modulesDictionary).forEach(moduleKey => {
     const moduleValue = modulesDictionary[moduleKey]
     const isNamespaced = Object.keys(moduleValue).includes('namespaced')
 
+    mockStoreActions(moduleKey, moduleValue, actionsDictionary, isNamespaced, jestFn)
+    mockStoreGetters(moduleKey, moduleValue, gettersDictionary, isNamespaced, jestFn, mockedGetters)
+  })
+
+  return {
+    actions: actionsDictionary,
+    getters: gettersDictionary
+  }
+}
+
+function mockStoreActions (moduleKey, moduleValue, actionsDictionary, isNamespaced, jestFn) {
+  if (moduleValue.actions) {
     Object.keys(moduleValue.actions).forEach(key => {
       const mockActionKey = isNamespaced ? `${moduleKey}/${key}` : key
       const mockFn = jestFn().mockResolvedValue({})
       actionsDictionary[mockActionKey] = mockFn
       moduleValue.actions[key] = mockFn
     })
+  }
+}
 
+function mockStoreGetters (moduleKey, moduleValue, gettersDictionary, isNamespaced, jestFn, mockedGetters) {
+  if (moduleValue.getters) {
     Object.keys(moduleValue.getters).forEach(key => {
       const mockedGetterKey = isNamespaced ? `${moduleKey}/${key}` : key
       let mockFn = jestFn()
@@ -27,11 +43,6 @@ function mockStoreActionsAndGetters ({
       gettersDictionary[mockedGetterKey] = mockFn
       moduleValue.getters[key] = mockFn
     })
-  })
-
-  return {
-    actions: actionsDictionary,
-    getters: gettersDictionary
   }
 }
 
